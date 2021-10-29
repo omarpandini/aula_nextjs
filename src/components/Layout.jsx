d27875f9365e6dd5bc55514db85d8be490b36706
ops01 <ops01@unimedchu.com.br>
2021-10-29 17:13:17 -0300

    commmit 2

---
+++ src/components/Layout.jsx
@@ -0,0 +1,18 @@
+import Link from "next/link"
+import Styles from "../styles/Layout.module.css"
+
+export default function Layout(props) {
+    console.log(props);
+    return(
+        <div className={Styles.layout}>
+
+            <div className={Styles.cabecalho}>
+                <Link href="/">Voltar</Link>
+            </div>
+
+            <div className={Styles.conteudo}>
+                {props.children}
+            </div>           
+        </div>
+    )
+}
--- src/pages/estiloso.jsx
+++ src/pages/estiloso.jsx
@@ -1,9 +1,13 @@
 import styles from '../styles/Estiloso.module.css'
+import Link from 'next/link';
+import Layout from '../components/Layout'
 
 export default function Estiloso(params) {
     return (
+        <Layout>
         <div className={styles.roxo}>
             <h1>Estilo usando CSS Módulos</h1>
         </div>
+        </Layout>
     )    
 }
